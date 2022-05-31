3e28741256f2b40b4a7fbc9464c65010397f1675
Carlos Jimenez <carlosjime512@gmail.com>
2022-05-31 13:13:35 -0500

    GifGridItem.js actualizado

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -1,8 +1,6 @@
 import React from 'react'
 
 export const GifGridItem = ({ title, url }) => {
-    
-    console.log( title, url);
 
     return (
         <div className="card animate__animated animate__fadeIn">
